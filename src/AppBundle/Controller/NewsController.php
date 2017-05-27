d7b30b950f1303a180f2afb31c86f250916c456f
Michael Trotzer <michael.trotzer@googlemail.com>
2017-05-27 09:50:24 +0200

    Ist leider alles in einer Datei war schon zu Spät :S

--- src/AppBundle/Controller/NewsController.php
+++ src/AppBundle/Controller/NewsController.php
@@ -8,25 +8,13 @@
 
 namespace AppBundle\Controller;
 
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Response;
 
 class NewsController extends Controller
 {
-    /**
-     * @Route("/News/{Titel}/{Text}")
-     */
-    public function showAction($Titel, $Text)
+    public function showAction()
     {
-        $Titel = "Hier ist die Überschrift";
-        $Text = "Hallo hier folgt der Text";
-        $templating = $this->container->get('templating');
-        $html = $templating->render('default/show.html.twig', [
-            'news' => $Titel,
-            'text' => $Text
-    ]);
-        return new Response($html);
+        return $this->render('home.html.twig');
 
     }
 }
