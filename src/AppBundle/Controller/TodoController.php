ad8131004d1dcc71fa8875250bc90a330850cf9d
Michael Trotzer <michael.trotzer@googlemail.com>
2017-05-31 06:40:34 +0200

    Todo-Anwendung weiter gepflegt

--- src/AppBundle/Controller/TodoController.php
+++ src/AppBundle/Controller/TodoController.php
@@ -8,9 +8,20 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Todo;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Include everything for the Forms
+ */
+
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class todoController extends Controller
 {
@@ -19,7 +30,13 @@ class todoController extends Controller
      */
     public function listAction()
     {
-        return $this->render('todo/index.html.twig');
+        $todos = $this->getDoctrine()
+            ->getRepository('AppBundle:Todo')
+            ->findAll();
+
+        return $this->render('todo/index.html.twig', array(
+            'todos' => $todos
+        ));
     }
 
     /**
@@ -27,7 +44,52 @@ class todoController extends Controller
      */
     public function createAction(Request $request)
     {
-        return $this->render('todo/create.html.twig');
+        $todo = new Todo;
+
+        $form =$this->createFormBuilder($todo)
+            ->add('name', TextType::class, array('attr' => array('class' => 'form-control', 'style' =>'margin-bottom:15px')))
+            ->add('category', TextType::class, array('attr' => array('class' => 'form-control', 'style' =>'margin-bottom:15px')))
+            ->add('description', TextareaType::class, array('attr' => array('class' => 'form-control', 'style' =>'margin-bottom:15px')))
+            ->add('priority', ChoiceType::class, array('choices' => array('Low' => 'Low', 'Normal' => 'Normal', 'High' => 'High'), 'attr' => array('class' => 'form-control', 'style' =>'margin-bottom:15px')))
+            ->add('dueDate', DateTimeType::class, array('attr' => array('class' => 'formcontrol', 'style' =>'margin-bottom:15px')))
+            ->add('save', SubmitType::class, array('label' => 'Create Todo', 'attr' => array('class' => 'btn btn-primary', 'style' =>'margin-bottom:15px')))
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            //getData
+            $name = $form['name']->getData();
+            $category = $form['category']->getData();
+            $description = $form['description']->getData();
+            $priority = $form['priority']->getData();
+            $dueDate = $form['dueDate']->getData();
+
+            $now = new\DateTime('now');
+
+            $todo->setName($name);
+            $todo->setCategory($category);
+            $todo->setDescription($description);
+            $todo->setPriority($priority);
+            $todo->setDueDate($dueDate);
+            $todo->setCreateDate($now);
+
+            $em = $this->getDoctrine()->getManager();
+
+            $em->persist($todo);
+            $em->flush();
+
+            $this->addFlash(
+                'notice',
+                'Todo Added'
+            );
+
+            return $this->redirectToRoute('todo_list');
+        }
+
+        return $this->render('todo/create.html.twig', array(
+            'form' => $form->createView()
+        ));
     }
 
     /**
@@ -35,6 +97,7 @@ class todoController extends Controller
      */
     public function editAction($id, Request $request)
     {
+
         return $this->render('todo/edit.html.twig');
     }
 
@@ -43,6 +106,12 @@ class todoController extends Controller
      */
     public function detailsAction($id)
     {
-        return $this->render('todo/details.html.twig');
+        $todos = $this->getDoctrine()
+            ->getRepository('AppBundle:Todo')
+            ->find($id);
+
+        return $this->render('todo/details.html.twig', array(
+            'todos' => $todos
+        ));
     }
 }
