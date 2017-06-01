0d34cd4a7cf842c611d05c65db8f1a1d7c013e36
Michael Trotzer <michael.trotzer@googlemail.com>
2017-06-01 02:05:13 +0200

    News weiter rumgespielt

--- src/AppBundle/Controller/newsController.php
+++ src/AppBundle/Controller/newsController.php
@@ -11,6 +11,15 @@ namespace AppBundle\Controller;
 use AppBundle\Entity\News;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Include everything for the Forms
+ */
+
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 
 class newsController extends Controller
 {
@@ -25,6 +34,52 @@ class newsController extends Controller
 
         return $this->render('news/index.html.twig', array(
             'news' => $news
+        ));
+    }
+
+    /**
+     * @Route("/news/create", name="news_create")
+     */
+    public function createAction(Request $request)
+    {
+        $news = new news;
+
+        $form =$this->createFormBuilder($news)
+            ->add('article', TextareaType::class, array('attr' => array('class' => 'form-control', 'style' =>'margin-bottom:15px')))
+            ->add('author', TextType::class, array('attr' => array('class' => 'form-control', 'style' =>'margin-bottom:15px')))
+            ->add('publicationdate', DateTimeType::class, array('attr' => array('class' => 'formcontrol', 'style' =>'margin-bottom:15px')))
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            //getData
+            $article = $form['article']->getData();
+            $author = $form['author']->getData();
+            $publicationdate = $form['publicationdate']->getData();
+
+            $now = new\DateTime('now');
+
+            $news->setArticle($article);
+            $news->setAuthor($author);
+            $news->setPublicationdate($publicationdate);
+            $news->setCreateDate($now);
+
+            $em = $this->getDoctrine()->getManager();
+
+            $em->persist($news);
+            $em->flush();
+
+            $this->addFlash(
+                'notice',
+                'News Added'
+            );
+
+            return $this->redirectToRoute('news_list');
+        }
+
+        return $this->render('news/create.html.twig', array(
+            'form' => $form->createView()
         ));
     }
 }
