f07147ecb26d418ffc1506fcff80d77a3c8a6a83
Michael Trotzer <michael.trotzer@googlemail.com>
2017-06-06 09:21:59 +0200

    Mitarbeiterdatenbank angelegt muss noch weiter gepflegt werden aber nimmt Formen an!

--- src/AppBundle/Controller/mitarbeiterController.php
+++ src/AppBundle/Controller/mitarbeiterController.php
@@ -22,7 +22,8 @@ use Symfony\Component\HttpFoundation\Request;
 
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 
 class mitarbeiterController extends Controller
 {
@@ -44,21 +45,52 @@ class mitarbeiterController extends Controller
         $form = $this->createFormBuilder($mitarbeiter)
             ->add('vorname', TextType::class, array('attr' => array('class' => 'form-control', 'style' => 'margin-bottom:15px')))
             ->add('nachname', TextType::class, array('attr' => array('class' => 'form-control', 'style' => 'margin-bottom:15px')))
-            ->add('abteilung', TextType::class, array('attr' => array('class' => 'form-control', 'style' => 'margin-bottom:15px')))
+            ->add('strasse', TextType::class, array('attr' => array('class' => 'form-control', 'style' => 'margin-bottom:15px')))
+            ->add('hausnummer', NumberType::class, array('attr' => array('class' => 'form-control', 'style' => 'margin-bottom:15px')))
+            ->add('postleitzahl', NumberType::class, array('attr' => array('class' => 'form-control', 'style' => 'margin-bottom:15px')))
+            ->add('ort', NumberType::class, array('attr' => array('class' => 'form-control', 'style' => 'margin-bottom:15px')))
+            ->add('geburtsdatum', NumberType::class, array('attr' => array('class' => 'form-control', 'style' => 'margin-bottom:15px')))
+            ->add('geburtsort', TextType::class, array('attr' => array('class' => 'form-control', 'style' => 'margin-bottom:15px')))
+            ->add('personalausweissnummer', TextType::class, array('attr' => array('class' => 'form-control', 'style' => 'margin-bottom:15px')))
+            ->add('familienstand', ChoiceType::class, array('choices' => array('ledig' => 'ledig', 'verheiratet' => 'verheiratet', 'High' => 'High'), 'attr' => array('class' => 'form-control', 'style' =>'margin-bottom:15px')))
+
+            ->add('saveAndAdd', SubmitType::class, array('label' => 'Speichern und fortführen', 'attr' => array('class' => 'btn btn-primary', 'style' =>'margin-bottom:15px')))
             ->add('save', SubmitType::class, array('label' => 'Erstelle Mitarbeiter', 'attr' => array('class' => 'btn btn-primary', 'style' =>'margin-bottom:15px')))
             ->getForm();
 
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+
+            // Speichern und fortführen Button
+            $nextAction = $form->get('saveAndAdd')->isClicked()
+                ? 'task_new'
+                : 'task_success';
+
+            return $this->redirectToRoute($nextAction);
+
             //getData
             $vorname = $form['vorname']->getData();
             $nachname = $form['nachname']->getData();
-            $abteilung = $form['abteilung']->getData();
+            $strasse = $form['strasse']->getData();
+            $hausnummer = $form['hausnummer']->getData();
+            $postleitzahl = $form['postleitzahl']->getData();
+            $ort = $form['ort']->getData();
+            $geburtsdatum = $form['geburtsdatum']->getData();
+            $geburtsort = $form['geburtsort']->getData();
+            $familienstand = $form['familienstand']->getData();
+            $personalausweissnummer = $form['personalausweissnummer']->getData();
 
             $mitarbeiter->setVorname($vorname);
             $mitarbeiter->setNachname($nachname);
-            $mitarbeiter->setAbteilung($abteilung);
+            $mitarbeiter->setStrasse($strasse);
+            $mitarbeiter->setHausnummer($hausnummer);
+            $mitarbeiter->setPostleitzahl($postleitzahl);
+            $mitarbeiter->setOrt($ort);
+            $mitarbeiter->setGeburtsdatum($geburtsdatum);
+            $mitarbeiter->setGeburtsort($geburtsort);
+            $mitarbeiter->setFamilienstand($familienstand);
+            $mitarbeiter->setPersonalausweissnummer($personalausweissnummer);
 
             $em = $this->getDoctrine()->getManager();
 
@@ -67,7 +99,7 @@ class mitarbeiterController extends Controller
 
             $this->addFlash(
                 'notice',
-                'Mitarbeiter Updated'
+                'Der Mitarbeiter wurde erstellt!'
             );
 
             return $this->redirectToRoute('Mitarbeiter_erstellen');
